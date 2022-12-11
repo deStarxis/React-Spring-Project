40a42c1f43f791c3e2d6639dad784d6cdbef0d5d
deStarxis <humagainuzjwol@gmail.com>
2022-12-10 20:09:43 -0600

    added login

--- spring-security-frontend/src/pages/Login.js
+++ spring-security-frontend/src/pages/Login.js
@@ -1,29 +1,70 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Navigate, redirect, useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
 function Login() {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+  const [loginState, setLoginState] = useState(initialState);
+  const navigate = useNavigate();
+  const onLoginClicked = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/uaa/login",
+        loginState
+      );
+      localStorage.setItem("accessToken", response.data.accessToken);
+      toast.success("Login Successful", {
+        autoClose: 1000,
+      });
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // const accessToken = localStorage.getItem("accessToken");
+  // if (accessToken) {
+  //   return redirect("/");
+  // }
   return (
-    <form style={{ width: "50%", margin: "auto", marginTop: "30px" }}>
-      <div class="form-group">
-        <label for="email">Email address</label>
+    <form
+      method="POST"
+      style={{ width: "50%", margin: "auto", marginTop: "30px" }}
+      onSubmit={onLoginClicked}
+    >
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="email"
+          name="email"
+          onChange={(e) =>
+            setLoginState({ ...loginState, email: e.target.value })
+          }
           placeholder="Enter email"
           required
         />
       </div>
-      <div class="form-group">
-        <label for="password">Password</label>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
         <input
           type="password"
-          class="form-control"
+          className="form-control"
           id="password"
+          name="password"
+          onChange={(e) =>
+            setLoginState({ ...loginState, password: e.target.value })
+          }
           placeholder="Password"
           required
         />
       </div>
-      <button type="submit" class="btn btn-primary">
+      <button type="submit" className="btn btn-primary">
         Login
       </button>
     </form>
